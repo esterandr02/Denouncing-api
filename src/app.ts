8240cb01e67279b67a69fe6a3f343f15ea1f6958
esterandr02 <esterandr02@gmail.com>
2020-08-30 12:46:45 -0300

    servidor rodando

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('🌐️ Server started on port 3000!');
+});
