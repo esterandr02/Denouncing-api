341893018c9e3b011696645367a225f1c65e2658
esterandr02 <esterandr02@gmail.com>
2020-08-31 15:56:00 -0300

    adicionada a abstracao da interface de address

--- src/dto/Response.ts
+++ src/dto/Response.ts
@@ -1,3 +1,5 @@
+import Address from './Address';
+
 interface Whistleblower {
     name: string;
     cpf: string;
@@ -6,15 +8,6 @@ interface Whistleblower {
 interface Complaint {
     title: string;
     description: string;
-}
-
-interface Address {
-    street: string;
-    neighborhood: string;
-    city: string;
-    state: string;
-    country: string;
-    cep: string;
 }
 
 export default interface Response {
