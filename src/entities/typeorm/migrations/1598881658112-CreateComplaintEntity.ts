743c5075b63ad89d771f21f3d027af7f01019ec8
esterandr02 <esterandr02@gmail.com>
2020-09-01 00:03:47 -0300

    conexao do typeorm com o banco

---
+++ src/entities/typeorm/migrations/1598881658112-CreateComplaintEntity.ts
@@ -0,0 +1,64 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateComplaintEntity1598881658112 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'complaints',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'title',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'whistleblower_id',
+                        type: 'uuid',
+                    },
+                    {
+                        name: 'latitude',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'longitude',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        name: 'UserComplaints',
+                        referencedTableName: 'whistleblower',
+                        referencedColumnNames: ['id'],
+                        columnNames: ['whistleblower_id'],
+                        onDelete: 'CASCADE',
+                        onUpdate: 'CASCADE',
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('complaints');
+    }
+}
